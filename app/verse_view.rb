d18cd89a966fb2e1b6137fe6172b71b196e9fd57
Rich Kilmer <rich.kilmer@gmail.com>
2015-04-18 09:41:09 -0400

    add chapter panning and selection indicator

--- app/verse_view.rb
+++ app/verse_view.rb
@@ -1,9 +1,21 @@
 class VerseView < UIView
 
-  attr_reader :verse, :labels, :position, :verseLabels
+  attr_reader :verse, :labels, :position, :verseLabels, :verseReferenceLabel, :verseScroller, :selectorView
   attr_accessor :delegate
   
-  VerticalOffset = 100
+  VerticalOffset = 50
+  
+  def slidePosition
+    @position = position - 1
+    UIView.animateWithDuration(0.5,
+        animations: lambda {
+           self.frame = [[20, position*80 + VerticalOffset], [UIScreen.mainScreen.bounds.size.width-40, 50]]
+        },
+        completion:lambda {|finished|
+          
+        }
+      )
+  end
 
   def initWithVerse(verse, position: position)
     @verse = verse
@@ -15,7 +27,26 @@ class VerseView < UIView
     @labels = []
     @verseLabels = []
     
+    @selectorView = UIView.alloc.init
+    @selectorView.backgroundColor = color(:white)
+    @selectorView.frame = [[0,0],[5,frame.size.height]]
+    @selectorView.clipsToBounds = true
+    addSubview selectorView
+    
+    @verseScroller = UIView.alloc.init
+    @verseScroller.backgroundColor = color(:white)
+    @verseScroller.frame = [[10,0],[frame.size.width-10,frame.size.height]]
+    @verseScroller.clipsToBounds = true
+    addSubview verseScroller
+    
+    @verseReferenceLabel = UILabel.alloc.init
+    verseReferenceLabel.frame = [[0,0], [0,0]]
+    verseReferenceLabel.font = font(:verse_reference)
+    verseReferenceLabel.color = color(:verse_reference)
+    verseScroller.addSubview verseReferenceLabel
+    
     createLabels
+    updateVerseReference
     
     clickButton = UIButton.buttonWithType(UIButtonTypeCustom)
     clickButton.addTarget self, action:"press:", forControlEvents:UIControlEventTouchUpInside
@@ -37,6 +68,12 @@ class VerseView < UIView
     @verseLabels = []
   end
   
+  def updateVerseReference
+    verseReferenceLabel.text = verse.to_s.upcase
+    verseReferenceLabel.size = verseReferenceLabel.sizeThatFits(CGSizeMake(10000, 40))
+    verseReferenceLabel.origin = CGPointMake(2, self.size.height - verseReferenceLabel.size.height + 2)
+  end
+  
   def createLabels
     v = verse.previous.previous
     5.times do
@@ -47,15 +84,16 @@ class VerseView < UIView
       label.frame = [[0,0], [0,0]]
       label.size = label.sizeThatFits(CGSizeMake(10000, 40))
       label.origin = CGPointMake(0, labelY)
-      addSubview label
+      verseScroller.addSubview label
       labels << label
       
       verseLabel = UILabel.alloc.init
       verseLabel.text = v.verse.to_s
       verseLabel.font = font(:bold, 13)
+      verseLabel.color = color(:verse_reference)
       verseLabel.frame = [[0,0], [0,0]]
       verseLabel.size = verseLabel.sizeThatFits(CGSizeMake(10000, 40))
-      addSubview verseLabel
+      verseScroller.addSubview verseLabel
       verseLabels << verseLabel
       
       v = v.next
@@ -81,16 +119,16 @@ class VerseView < UIView
       label.font = font(:base, 24)
       label.frame = [[0,0], [0,0]]
       label.size = label.sizeThatFits(CGSizeMake(10000, 40))
-      (self.size.height - label.size.height)/2
+      (self.size.height - label.size.height)/2 - 7
     end
   end
   
   def select
-    self.backgroundColor = color(:yellow)  
+    selectorView.backgroundColor = color(:black)  
   end
   
   def deselect
-    self.backgroundColor = color(:white)
+    selectorView.backgroundColor = color(:white)
   end
   
   def press(button)
@@ -103,27 +141,44 @@ class VerseView < UIView
       label.origin = CGPointMake(x, labelY) 
     end
     alignVerseLabels
-    if labels[2].origin.x > size.width
-      offset = (labels[2].origin.x - size.width)
+    if (labels[2].origin.x + PADDING) > verseScroller.size.width
+      offset = labels[1].origin.x - PADDING
       previousVerse
       pan(offset)
     elsif labels[3].origin.x <= 0
-      offset = labels[3].origin.x
+      offset = labels[3].origin.x - PADDING
       nextVerse
       pan(offset)
     end
+    updateVerseReference
   end
   
   def nextVerse
     @verse = @verse.next
     removeLabels
     createLabels
+    updateVerseReference
   end
   
   def previousVerse
     @verse = @verse.previous
     removeLabels
     createLabels
+    updateVerseReference
+  end
+
+  def nextChapter
+    @verse = @verse.next_chapter
+    removeLabels
+    createLabels
+    updateVerseReference
+  end
+  
+  def previousChapter
+    @verse = @verse.previous_chapter
+    removeLabels
+    createLabels
+    updateVerseReference
   end
   
   private
